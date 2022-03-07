81b4a88d36b9e316aba6869107a310a850b7f772
HagerCha <hager.ch2@gmail.com>
2022-03-07 18:25:17 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { style } from '@angular/animations';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './index.html',
+  styleUrls: ['./css/animate.css',
+              './css/bootstrap-datepicker.min.css',
+              './css/bootstrap.css',
+              './css/cs-select.css',
+              './css/cs-skin-border.css',
+              './css/icomoon.css',
+              './css/magnific-popup.css',
+              './css/style.css',
+              './css/superfish.css']
+  
+})
+export class AppComponent {
+  title = 'Welcome';
+}
+
